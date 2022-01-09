8f56c8c39f3a3e1fabb5e721c6a5152f85a13f1a
Andreas Rozek <A.Rozek@gmx.de>
2022-01-09 06:43:15 +0100

    Create Demo.js

---
+++ Demo.js
@@ -0,0 +1,15 @@
+  g.setBgColor('#000000');
+  g.clear(false);
+
+  require('https://raw.githubusercontent.com/rozek/banglejs-2-fillring/main/fillRing.js');
+
+  let ScreenWidth  = g.getWidth(),  CenterX = ScreenWidth/2;
+  let ScreenHeight = g.getHeight(), CenterY = ScreenHeight/2;
+
+  let outerRadius = Math.min(CenterX,CenterY) * 0.9;
+  let innerRadius = outerRadius * 0.8;
+
+  g.clear();
+
+  g.setColor('#FFFF00');
+  g.fillRing(CenterX,CenterY, outerRadius,innerRadius);
